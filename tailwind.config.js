e5907b576d0da0751a4e2d0b1d70068865c6624c
Pankorn Chantarod <pankorn.ch@gmail.com>
2021-12-29 01:00:23 +0700

    :lipstick: index page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
 module.exports = {
 	content: [
 		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./components/**/*.{js,ts,jsx,tsx}",
+		"./src/components/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				main: {
+					"blue": "#2680fe",
+					"blue-light": "#9DC6FF",
+					"light": "#E6EEFF",
+					"dark": "#324567",
+					"dark-light": "#5A719D",
+					"red": "#E84444",
+					"green": "#2FCE2C"
+				}
+			}
+		},
 	},
 	plugins: [],
+	corePlugins: {
+		container: false,
+	},
+	mode: 'jit',
 }
